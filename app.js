8001ac357af2fadebf52b69fb3640dfda22b592d
Alican Metin <alicanmtn@gmail.com>
2024-07-10 20:23:43 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+window.addEventListener("load", async () => {
+  const subscribeButton = document.querySelector("#subscribeButton");
+
+  //register service worker
+  const sW = await navigator.serviceWorker.register("./sw.js");
+  console.log("Service Worker => ", sW);
+
+  subscribeButton.addEventListener("click", async () => {
+    const serviceWorker = await navigator.serviceWorker.ready;
+    const clientId = await serviceWorker.pushManager.subscribe({
+      userVisibleOnly: true,
+      applicationServerKey:
+        "BADjUo07NGwAYB8hW3eaHbFgg-7JRo_NrCrXRzvd4n2ypR86KiXb9Pu5p6yVoeO9jdUWNGQMfRlewxpQ3FvJs98",
+    });
+
+    console.log(clientId);
+    console.log(JSON.stringify(clientId));
+  });
+});
---
+++ sw.js
@@ -0,0 +1,16 @@
+//sunucu üzerinden gelecek notificationları bize gösterecek yer
+
+//bir push eventi meydana gelince bana bunu ver
+self.addEventListener("push", (e) => {
+  const config = {
+    body: "mesaj içeriği burada yer alacak.",
+    data: {
+      dateOfArrival: Date.now(),
+      primaryKey: "3",
+    },
+  };
+
+  e.waitUntil(
+    self.registration.showNotification("Yeni Makale Eklendi!!", config)
+  );
+});
