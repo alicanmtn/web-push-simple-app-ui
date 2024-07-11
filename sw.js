96184c8eab105a0e92e408f004e1d26d66448aec
Alican Metin <alicanmtn@gmail.com>
2024-07-11 08:24:22 +0300

    sw updated

--- sw.js
+++ sw.js
@@ -2,6 +2,8 @@
 
 //bir push eventi meydana gelince bana bunu ver
 self.addEventListener("push", (e) => {
+  console.log("e => ", e);
+
   const config = {
     body: "mesaj içeriği burada yer alacak.",
     data: {
