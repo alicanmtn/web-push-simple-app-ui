5bb849d0acf49fc73dd075d4e2c13ff3aaca9ba7
Alican Metin <alicanmtn@gmail.com>
2024-09-03 11:12:25 +0300

    sw changes v1.2

--- app.js
+++ app.js
@@ -5,13 +5,29 @@ window.addEventListener("load", async () => {
   const sW = await navigator.serviceWorker.register("./relatedpush_sw.js");
   console.log("Service Worker => ", sW);
 
+  let urlB64ToUint8Array = (base64String) => {
+    const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
+    const base64 = (base64String + padding)
+      .replace(/\-/g, "+")
+      .replace(/_/g, "/");
+
+    const rawData = window.atob(base64);
+    const outputArray = new Uint8Array(rawData.length);
+
+    for (let i = 0; i < rawData.length; ++i) {
+      outputArray[i] = rawData.charCodeAt(i);
+    }
+    return outputArray;
+  };
+
   subscribeButton.addEventListener("click", async () => {
     const serviceWorker = await navigator.serviceWorker.ready;
+    const SERVERKEY =
+      "BK3BKsyBoxXPQs8ID2OP1DkOhZOu7U_sNeKVJP-j75xSWzgdYosdYp9UyjUWA0-YgTsCX1KdU0qMxIOLBc1vMNU";
     const clientId = await serviceWorker.pushManager
       .subscribe({
         userVisibleOnly: true,
-        applicationServerKey:
-          "BK3BKsyBoxXPQs8ID2OP1DkOhZOu7U_sNeKVJP-j75xSWzgdYosdYp9UyjUWA0-YgTsCX1KdU0qMxIOLBc1vMNU",
+        applicationServerKey: urlB64ToUint8Array(SERVERKEY),
       })
       .then(() => {
         console.log(clientId);
