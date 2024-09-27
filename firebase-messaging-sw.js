e5c82e7c90ef40d3e06c5fe8282183a74353d62b
alican_metin <alican.metin@euromsg.com>
2024-09-27 14:14:03 +0300

    updatev6

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,4 +1,7 @@
-﻿//importScripts('https://www.gstatic.com/firebasejs/10.13.2/firebase-app.js');
+﻿importScripts('https://www.gstatic.com/firebasejs/8.0.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.0.0/firebase-messaging.js');
+
+//importScripts('https://www.gstatic.com/firebasejs/10.13.2/firebase-app.js');
 //importScripts('https://www.gstatic.com/firebasejs/10.13.2/firebase-messaging.js');
 
 //import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-app.js";
