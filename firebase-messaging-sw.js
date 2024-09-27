5d5d55e1960e3e5601a2cb7cdfb561e6deba7ccf
alican_metin <alican.metin@euromsg.com>
2024-09-27 13:46:28 +0300

    lütfen ol artık

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,9 +1,11 @@
-﻿importScripts('https://www.gstatic.com/firebasejs/10.13.2/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/10.13.2/firebase-messaging.js');
+﻿//importScripts('https://www.gstatic.com/firebasejs/10.13.2/firebase-app.js');
+//importScripts('https://www.gstatic.com/firebasejs/10.13.2/firebase-messaging.js');
 
 //import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-app.js";
 //import { firebase } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-messaging.js";
 
+importScripts("/firebaseJs/firebase-app.js");
+importScripts("/firebaseJs/firebase-messaging.js");
 
 //firebase.initializeApp({
 //    apiKey: "YOUR_API_KEY",
