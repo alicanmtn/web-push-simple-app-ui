73b3ba8271c35d1bf1700861504ca3bf48ea4159
alican_metin <alican.metin@euromsg.com>
2024-09-27 11:05:06 +0300

    update v2

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,14 +1,14 @@
 ﻿importScripts('https://www.gstatic.com/firebasejs/8.0.0/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/8.0.0/firebase-messaging.js');
 
-firebase.initializeApp({
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT_ID.appspot.com",
-    messagingSenderId: "YOUR_SENDER_ID",
-    appId: "YOUR_APP_ID"
-});
+//firebase.initializeApp({
+//    apiKey: "YOUR_API_KEY",
+//    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+//    projectId: "YOUR_PROJECT_ID",
+//    storageBucket: "YOUR_PROJECT_ID.appspot.com",
+//    messagingSenderId: "YOUR_SENDER_ID",
+//    appId: "YOUR_APP_ID"
+//});
 
 const messaging = firebase.messaging();
 
@@ -22,11 +22,11 @@ messaging.requestPermission().then(function () {
     console.log('Bildirim iznine erişilemedi:', err);
 });
 
-Notification.requestPermission().then(permission => {
-    if (permission === 'granted') {
-        console.log('Bildirim izni verildi.');
-        // Token alma ve sunucuya gönderme işlemleri
-    } else {
-        console.log('Bildirim izni verilmedi.');
-    }
-});
+//Notification.requestPermission().then(permission => {
+//    if (permission === 'granted') {
+//        console.log('Bildirim izni verildi.');
+//        // Token alma ve sunucuya gönderme işlemleri
+//    } else {
+//        console.log('Bildirim izni verilmedi.');
+//    }
+//});
