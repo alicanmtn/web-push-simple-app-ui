8b1e59e7ac39010e5e4292168f3a55eab546c087
alican_metin <alican.metin@euromsg.com>
2024-09-27 14:23:02 +0300

    upv8

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -10,14 +10,6 @@
 importScripts("/firebaseJs/firebase-app.js");
 importScripts("/firebaseJs/firebase-messaging.js");
 
-//firebase.initializeApp({
-//    apiKey: "YOUR_API_KEY",
-//    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-//    projectId: "YOUR_PROJECT_ID",
-//    storageBucket: "YOUR_PROJECT_ID.appspot.com",
-//    messagingSenderId: "YOUR_SENDER_ID",
-//    appId: "YOUR_APP_ID"
-//});
 const firebaseConfig = {
     apiKey: "AIzaSyAm8lQv0jp-zNaDuqpEYvOXnzU-vYyG-n0",
     authDomain: "test-project-for-notific-b60f2.firebaseapp.com",
