b59943079b78f3baf99fa493c770287c3f373e1d
Alican Metin <alicanmtn@gmail.com>
2024-09-30 22:20:22 +0300

    update2

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-import path from "path";
+var path = require("path");
 
 let app_base = path.dirname(require.main.filename);
 
--- createtemplate.js
+++ createtemplate.js
@@ -1,5 +1,5 @@
-﻿import fs from "fs";
-import config from "./config";
+﻿const fs = require("fs");
+var { config } = require("./config");
 
 var sdk_path = config.path.sdkpath;
 var template = config.path.output.template;
