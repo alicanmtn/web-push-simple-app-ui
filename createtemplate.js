0d44c26a694ea258c9c697c086b5975280a0feca
Alican Metin <alicanmtn@gmail.com>
2024-09-30 22:08:03 +0300

    update1

--- createtemplate.js
+++ createtemplate.js
@@ -1,5 +1,5 @@
-﻿const fs = require('fs');
-var { config } = require('./config');
+﻿import fs from "fs";
+import config from "./config";
 
 var sdk_path = config.path.sdkpath;
 var template = config.path.output.template;
@@ -11,45 +11,41 @@ let settingsString = ReadSettings();
 let sdkString = ReadSdk();
 
 if (settingsString && sdkString) {
-    WriteTemplate(settingsString + sdkString);
-    WriteBase64Template(settingsString + sdkString);
+  WriteTemplate(settingsString + sdkString);
+  WriteBase64Template(settingsString + sdkString);
 }
 function ReadSdk() {
-    if (!fs.existsSync(sdk_path))
-        throw Error("sdk javascript yok" + "_" + sdk_path);
+  if (!fs.existsSync(sdk_path))
+    throw Error("sdk javascript yok" + "_" + sdk_path);
 
-    let content = fs.readFileSync(sdk_path, 'utf8');
-    let res = content.split(splitter);
-    if (res.length != 2) {
-        console.log("hata split karakteri yok");
-        return undefined;
-    }
-    return res[1];
+  let content = fs.readFileSync(sdk_path, "utf8");
+  let res = content.split(splitter);
+  if (res.length != 2) {
+    console.log("hata split karakteri yok");
+    return undefined;
+  }
+  return res[1];
 }
 
 function ReadSettings() {
-    if (!fs.existsSync(settings))
-        throw Error("setting template yok");
-    let content = fs.readFileSync(settings, 'utf8');
-    if (content !== "" || content != undefined)
-        return content;
-    return undefined;
+  if (!fs.existsSync(settings)) throw Error("setting template yok");
+  let content = fs.readFileSync(settings, "utf8");
+  if (content !== "" || content != undefined) return content;
+  return undefined;
 }
 
 function WriteTemplate(str) {
-    fs.writeFile(template, str, 'utf8', (err, data) => {
-        if (err) throw err;
-        console.log(`template oluşturuldu. path: ${template}`);
-    });
-
+  fs.writeFile(template, str, "utf8", (err, data) => {
+    if (err) throw err;
+    console.log(`template oluşturuldu. path: ${template}`);
+  });
 }
 
 function WriteBase64Template(str) {
-    const buff = Buffer.from(str);
-    var text = buff.toString("base64");
-    fs.writeFile(tempale_base64, text, 'utf8', (err, data) => {
-        if (err) throw err;
-        console.log(`base64 template scripti oluşturuldu. path ${tempale_base64}`)
-    });
-
+  const buff = Buffer.from(str);
+  var text = buff.toString("base64");
+  fs.writeFile(tempale_base64, text, "utf8", (err, data) => {
+    if (err) throw err;
+    console.log(`base64 template scripti oluşturuldu. path ${tempale_base64}`);
+  });
 }
